d5c08b923b5aae446f5624b3211181df85cfed82
louisheimel1@gmail.com <louisheimel1@gmail.com>
2017-07-05 19:47:00 -0500

    make markdown previewer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const marked = require('marked')
+
+class ControlledInput extends Component {
+  render() {
+    return (<div className="App">
+      <h1>Markdown Previewer (using Marked library)</h1>
+      <p>Type text here: </p>
+      <textarea type="text" onChange={this.props.onChange} ></textarea>
+    </div>);
+  }
+}
+
+class DisplayText extends Component {
+  markup() {
+    return {__html: marked(this.props.text) };
+  }
+
+  render() {
+    return (
+      <div dangerouslySetInnerHTML={this.markup()}></div>
+    );
+  }
+}
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  onChange(e) {
+    this.setState({
+      text: e.target.value
+    }) 
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ControlledInput onChange={this.onChange.bind(this)} />
+        <DisplayText text={this.state.text} />
       </div>
     );
   }
